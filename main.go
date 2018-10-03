658e726aa99f726c02dc1eb6a3a933f44ce9ee42
xiachg <albertxiacg@gmail.com>
2018-10-03 17:07:50 +0800

    利用正则表达式获取城市列表和城市名

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
@@ -13,7 +14,7 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://www.zhenai.com/zhenghun/shanghai")
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
 	if err != nil {
 		panic(err)
 	}
@@ -31,9 +32,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("%s\n", all)
-
+	printCityList(all)
 }
 
 func determineEncoding(r io.Reader) encoding.Encoding {
@@ -44,4 +43,13 @@ func determineEncoding(r io.Reader) encoding.Encoding {
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
 
+}
+
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s URL: %s\n", m[2], m[1])
+	}
+	fmt.Printf("Matches found: %d\n", len(matches))
 }
