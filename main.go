12e56c2612744bac1596e3463e93acf141f87aa1
xiachg <albertxiacg@gmail.com>
2018-10-04 23:40:24 +0800

    1.添加数据存储 2.去除不必要的数据收集

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"./engine"
 
+	"./persist"
 	"./scheduler"
 	"./zhenai/parser"
 )
@@ -10,13 +11,21 @@ import (
 func main() {
 
 	e := engine.ConcurrentEngine{
-		Scheduler:   &scheduler.SimpleScheduler{},
+		Scheduler:   &scheduler.QueuedScheduler{},
 		WorkerCount: 100,
+		ItemChan:    persist.ItemSaver(),
 	}
 
+	// 获取所有城市
 	e.Run(engine.Request{
 		Url:        "http://www.zhenai.com/zhenghun",
 		ParserFunc: parser.ParseCityList,
 	})
+
+	// 获取单个城市
+	//e.Run(engine.Request{
+	//	Url:        "http://www.zhenai.com/zhenghun/shanghai",
+	//	ParserFunc: parser.ParseCity,
+	//})
 
 }
