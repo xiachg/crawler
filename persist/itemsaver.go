a4c9d502ba55172f9e11c98dd16caf62277c5474
xiachg <albertxiacg@gmail.com>
2018-10-05 23:05:28 +0800

    1.利用elastic search 存储用户数据 2.增加itemsaver_test 测试用例 3.利用docker安装elasticsearch 数据库 4.修复一个数据存储中一个int类型存储Bug

--- persist/itemsaver.go
+++ persist/itemsaver.go
@@ -1,6 +1,11 @@
 package persist
 
-import "log"
+import (
+	"context"
+	"log"
+
+	"gopkg.in/olivere/elastic.v5"
+)
 
 func ItemSaver() chan interface{} {
 
@@ -14,9 +19,34 @@ func ItemSaver() chan interface{} {
 			item := <-out
 			log.Printf("Item Saver: got item "+"#%d: %v", itemCount, item)
 			itemCount++
+
+			_, err := save(item)
+
+			if err != nil {
+				log.Printf("Item Saver: error "+"saving item %v: %v", item, err)
+			}
+
 		}
 
 	}()
 
 	return out
+}
+
+func save(item interface{}) (id string, err error) {
+
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := client.Index().Index("dating_profile").Type("zhenai").BodyJson(item).Do(context.Background())
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Id, nil
+
 }
